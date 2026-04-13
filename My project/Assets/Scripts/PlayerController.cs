1c5e0cb26f35e6837749680a5de56ab3a2b685ee
IceBear2491 <salvadorparajon5@gmail.com>
2026-04-13 15:29:25 -0700

    sdas
    Added movement*

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ public class PlayerController : MonoBehaviour
 {
     private float speed = 10.0f;
     private Rigidbody playerRb;
+    public bool gameOver = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,17 +15,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            playerRb.AddForce(Vector3.left * speed * horizontalInput);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            playerRb.AddForce(Vector3.right * speed * horizontalInput);
-        }
 
     }
 }
